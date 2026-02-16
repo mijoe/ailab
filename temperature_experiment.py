dc5b98cd1d2351a3689a2b7d34f4772beabb7884
Michael Jörg <michael.joerg@usd.de>
2026-02-16 11:50:12 +0000

    Output for Temperature Experiment

--- temperature_experiment.py
+++ temperature_experiment.py
@@ -1,7 +1,13 @@
 import ollama
 
-ollama.generate(model='llama3.2:3b', prompt='What is the name of the highest building in the world?', options=dict(temperature=0.0))['response']
+print('--- Temperature 0.0 ---')
+print(ollama.generate(model='llama3.2:3b', prompt='What is the name of the highest building in the world?',
+      options=dict(temperature=0.0))['response'])
 
-ollama.generate(model='llama3.2:3b', prompt='What is the name of the highest building in the world?', options=dict(temperature=0.7))['response']
+print('--- Temperature 0.7 ---')
+print(ollama.generate(model='llama3.2:3b', prompt='What is the name of the highest building in the world?',
+      options=dict(temperature=0.7))['response'])
 
-ollama.generate(model='llama3.2:3b', prompt='What is the name of the highest building in the world?', options=dict(temperature=1.2))['response']
+print('--- Temperature 1.2 ---')
+print(ollama.generate(model='llama3.2:3b', prompt='What is the name of the highest building in the world?',
+      options=dict(temperature=1.2))['response'])
